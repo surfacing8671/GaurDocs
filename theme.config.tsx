63d74e3e5523b5dc0d8c1727d125ae2749d48456
surfacing8671 <jaeweb8671@gmail.com>
2023-01-25 13:06:49 -0600

    c

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,34 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
+  
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/2ev96gcCCS',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: <span>Gaur Money Documents</span>,
   },
 }
+export default {
+  docsRepositoryBase: 'https://gaur-docs.vercel.app/',
+  banner: {
+    key: '2.0-release',
+    text: <a href="https://nextra.site" target="_blank">
+      🎉 Gaur Vaults are almost Live!
+    </a>,
+  },
+  head: (
+    <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="title" content="Gaur Money" />
+    <meta property="description" content="The future of DeFi" />
+  </>
+  ),
+}
 
-export default config
+
